1e046053ad260563a488b46ecd24ff32880e1dd4
Deepsha Menghani <deepshamenghani07@gmail.com>
2023-09-17 16:15:02 -0500

    Updated tests

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -3,17 +3,34 @@
 #' Provide the brief summary of package
 #' libraries on your machine
 #'
+#' @param sizes logical indicating whether or not to calculate library sizes, Default FALSE
+#'
 #' @return A 'data.frame' containing the count of packages in each
-#'          of the users libraries
+#'          of the users libraries and size of the libraries
 #' @export
 #'
 #' @examples
-#' lib_summary()
-lib_summary <- function() {
+#' lib_summary(sizes = TRUE)
+lib_summary <- function(sizes = FALSE) {
+
+  if(!is.logical(sizes)) {
+    stop("'sizes must be TRUE or FALSE.'")
+  }
+
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
   names(pkg_df) <- c("Library", "n_packages")
+
+  if(sizes) {
+    pkg_df$lib_size <- sapply(
+      pkg_df$Library,
+      function(x) {
+        sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+      }
+    )
+  }
+
   pkg_df
 }
 
